d22b814692960b693c6c5f8c2cd335bca2ebe37e
Pacuraru Fabian Virgil <101675637+PacuraruFabianVirgil@users.noreply.github.com>
2022-04-20 23:04:17 +0300

    Add files via upload

---
+++ Laboratory1/HelloWorld.java
@@ -0,0 +1,9 @@
+package pacuraru.fabian.lab1.ex3;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
