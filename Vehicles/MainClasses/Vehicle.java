f771a0078e96a1c4df160c6918a5e0fe0934f1dc
Pacuraru Fabian Virgil <101675637+PacuraruFabianVirgil@users.noreply.github.com>
2022-05-03 23:36:00 +0300

    Vehicles

---
+++ Vehicles/MainClasses/Vehicle.java
@@ -0,0 +1,31 @@
+package edu.utcn.lab12.vehicles;
+
+import java.util.Objects;
+
+public class Vehicle {
+    private String type;
+    private int weight;
+
+    public Vehicle(String type, int weight) {
+        this.type = type;
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vehicle vehicle = (Vehicle) o;
+        return weight == vehicle.weight &&
+                type.equals(vehicle.type);
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, weight);
+    }
+}
